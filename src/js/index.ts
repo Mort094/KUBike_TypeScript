b921fe355287a59cfc5d26353934608edc90d154
Mort094 <mort094@hotmail.com>
2020-11-26 12:31:54 +0100

    TEST

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,34 @@
+import Axios , {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
+
+let baseUrl: string = "http://mort-rest.azurewebsites.net/api/cycles";
+
+interface ICycle {
+    cycle_id: number;
+    cycle_name: string;
+    cycle_coordinates: string;
+    cycle_status_id: number;
+}
 new Vue({
-    // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
-    // Before the application runs this TypeScript file will be compiled into bundle.js
-    // which is included at the bottom of the html file.
-    el: "#app",
-    data: {
-        name: "",
-        greeting: ""
-    },
-    methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
+        el: "#app",
+        data:{
+            cycles: []
+        },
+        methods: {
+            helperGetAndShow(url: string) {
+                Axios.get<ICycle[]>(url)
+                .then((response: AxiosResponse<ICycle[]>) => {
+                    this.Bikes = response.data
+                })
+                .catch((error: AxiosError) => {
+                    alert(error.message)
+                })
             }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        },
+        getAllBikes(){
+            this.helperGetAndShow(baseUrl)
         }
-    }
 })
