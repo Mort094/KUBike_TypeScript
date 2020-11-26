87d8b274d64cb79f107882666895c38309c41655
Mort094 <mort094@hotmail.com>
2020-11-26 13:06:18 +0100

    VIRKER

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,34 +1,38 @@
-import Axios , {
+import axios, {
     AxiosResponse,
     AxiosError
 } from "../../node_modules/axios/index";
 
 
-let baseUrl: string = "http://mort-rest.azurewebsites.net/api/cycles";
+let baseUrl: string = "http://mort-rest.azurewebsites.net/api/cycles/"
 
 interface ICycle {
-    cycle_id: number;
-    cycle_name: string;
-    cycle_coordinates: string;
-    cycle_status_id: number;
+    cycle_id: number
+    cycle_name: string
+    cycle_coordinates: string
+    fk_cycle_status_id: number
 }
 new Vue({
-        el: "#app",
-        data:{
-            cycles: []
-        },
-        methods: {
-            helperGetAndShow(url: string) {
-                Axios.get<ICycle[]>(url)
+    el: "#app",
+    data: {
+        cycles: []
+    },
+    created(){
+        this.getAllBikes()
+    },
+    methods: {
+        helperGetAndShow(url: string) { // helper metode: getAllCar + getByVendor are very similar
+            axios.get<ICycle[]>(url)
                 .then((response: AxiosResponse<ICycle[]>) => {
-                    this.Bikes = response.data
+                    this.cycles = response.data
                 })
                 .catch((error: AxiosError) => {
-                    alert(error.message)
+                    //this.message = error.message
+                    alert(error.message) // https://www.w3schools.com/js/js_popup.asp
                 })
-            }
         },
-        getAllBikes(){
+        getAllBikes() {
             this.helperGetAndShow(baseUrl)
         }
+    }
 })
