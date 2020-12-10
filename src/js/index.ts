85e424b144bae3959ae8a85b5eb55c1e309fcfa9
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2020-12-10 13:59:02 +0100

    Update Validering
    UPdate! ting ting .... .

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,16 +6,20 @@ import axios, {
 // https://regex101.com/
 // (Regular expression)
 var mailformat = /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@+k+u+\.+d+k/;
-var indholderTal = /\d/;
-var indholdeSmallBogstaver = /.*[a-z].*/;
-var indeholdeBigBogstaver = /.*[A-Z].*/;
-var indholder8Tal = /\d\d\d\d\d\d\d\d/;
+var indholderTal = /[^\D]/;
+var indholderBogstaver = /^[a-zA-Z]+$/;
+var lengthPasswordvalid = /.{8,}/;
+var indholdeStoreBogstaver = /[A-Z]/;
+
+var testfortegn = /[@$!%*#?&]/
+
+var indholder8Tal = /^[0-9]{8}$/;
 
 let baseCycleUrl: string = "https://mort-rest.azurewebsites.net/api/cycles/"
 let baseUserUrl: string = "https://mort-rest.azurewebsites.net/api/users/"
 let baseTripUrl: string = "https://mort-rest.azurewebsites.net/api/trip/"
 let baseMessageUrl: string = "https://mort-rest.azurewebsites.net/api/messages/"
-
+//#region Interface
 interface ICycle {
     cycle_id: number
     cycle_name: string
@@ -46,7 +50,7 @@ interface IMessage {
     messages_besked: string
     messages_status: number
 }
-
+//#endregion
 new Vue({
     el: "#app",
     data: {
@@ -507,13 +511,46 @@ new Vue({
                 if (this.addData.user_firstname == '') throw 'emptyFirstName';
                 if (this.addData.user_lastname == '') throw 'emptylastName';
                 if (this.addData.user_mobile == '') throw 'emptyMobil';
-                if (this.addData.user_password.match(indholderTal)) throw 'nullIntPassword';
-                if (this.addData.user_password.match(indeholdeBigBogstaver)) throw 'bigStrPassword';
-                if (this.addData.user_firstname.match(indholdeSmallBogstaver)) throw 'smallStrFirstName';
-                if (this.addData.user_lastname.match(indholdeSmallBogstaver)) throw 'smallStrLastName';
-                if (8 == this.addData.user_mobile.length() && this.addData.user_mobile.match(indholder8Tal)) throw '8xIntMobile';
-                if (this.addData.user_email.match(mailformat)) throw 'kuMail';
-                if (8 <= this.addData.user_password.length() || this.addData.user_user_password.length() <= 32) throw 'passwordLength';
+                if (indholderTal.test(this.addData.user_password))
+                {}
+                else {throw 'nullIntPassword';}
+                if (indholdeStoreBogstaver.test(this.addData.user_password))
+                {}
+                else {throw 'bigStrPassword';}
+                
+                if (indholder8Tal.test(this.addData.user_mobile))
+                {}
+                else {throw '8xIntMobile';}
+                
+                if (testfortegn.test(this.addData.user_password))
+                {}
+                else {throw 'passwordNullSymbol';}
+
+               if (lengthPasswordvalid.test(this.addData.user_password))
+               { 
+                  
+               }
+               else{
+                   throw 'passwordLength';
+               }
+
+                if (indholderBogstaver.test(this.addData.user_firstname))
+                { 
+                   
+                }
+                else{
+                    throw 'smallStrFirstName';
+                }
+
+                if (indholderBogstaver.test(this.addData.user_lastname))
+                {}
+                else{
+                    throw 'smallStrLastName';
+                }
+
+                if (mailformat.test(this.addData.user_email))
+                {}
+                else {throw 'kuMail';}
             }
             catch (err) {
                 var errorNumber = err
@@ -546,57 +583,73 @@ new Vue({
                     document.getElementById('opret-name').className = "form-control";
                     document.getElementById('opret-lastname').className = "form-control";
                     document.getElementById('opret-phone').className = "form-control";
-
+                    // 12
                     if (errorNumber == 'emptyEmail') {
                         this.errorMessage = 'Du skal skrive noget i email';
                         document.getElementById('opret-email').className = "form-control error";
                     }
+                    // 11
                     if (errorNumber == 'emptyPassword') {
                         this.errorMessage = 'Du skal skrive et password';
                         document.getElementById('opret-password').className = "form-control error";
                     }
+                    // 10
                     if (errorNumber == 'emptyFirstName') {
                         this.errorMessage = 'Du skal skrive et fornavn';
                         document.getElementById('opret-name').className = "form-control error";
                     }
+                    // 9
                     if (errorNumber == 'emptylastName') {
                         this.errorMessage = 'Du skal skrive et efternavn';
                         document.getElementById('opret-lastname').className = "form-control error";
                     }
+                    // 8
                     if (errorNumber == 'emptyMobil') {
                         this.errorMessage = 'Du skal skrive et telefonnummer';
                         document.getElementById('opret-phone').className = "form-control error";
                     }
+                    // 7
                     if (errorNumber == 'nullIntPassword') {
                         this.errorMessage = 'Dit password skal indholde tal';
                         document.getElementById('opret-password').className = "form-control error";
                     }
+                    // 6
                     if (errorNumber == 'bigStrPassword') {
-                        this.errorMessage = 'Dit password skal indholde små bogstaver';
+                        this.errorMessage = 'Dit password skal indholde et stort bogstav';
                         document.getElementById('opret-password').className = "form-control error";
                     }
+                    // 5
                     if (errorNumber == 'smallStrFirstName') {
-                        this.errorMessage = 'Dit password skal indholde store bogstaver';
-                        document.getElementById('opret-password').className = "form-control error";
-                    }
-                    if (errorNumber == 'smallStrLastName') {
-                        this.errorMessage = 'Dit fornavn skal indholde bogstaver';
+                        this.errorMessage = 'Dit Navn må kun indholde bogstaver';
                         document.getElementById('opret-name').className = "form-control error";
                     }
-                    if (errorNumber == '8xIntMobile') {
-                        this.errorMessage = 'Dit efternavn skal indholde bogstaver';
+                    // 4 
+                    if (errorNumber == 'smallStrLastName') {
+                        this.errorMessage = 'Dit Efter navn skal indholde bogstaver';
                         document.getElementById('opret-lastname').className = "form-control error";
                     }
-                    if (errorNumber == 'kuMail') {
+                    // 3
+                    if (errorNumber == '8xIntMobile') {
                         this.errorMessage = 'Dit telefonnummer skal indholde == 8 tal';
                         document.getElementById('opret-phone').className = "form-control error";
                     }
-                    if (errorNumber == 'passwordLength') {
+                    // 2
+                    if (errorNumber == 'kuMail') {
                         this.errorMessage = 'Du skal bruge din skole email fx navn@ku.dk';
                         document.getElementById('opret-email').className = "form-control error";
                     }
+                    // 1
+                    if (errorNumber == 'passwordLength') {
+                        this.errorMessage = 'Dit password skal minimum være 8 tegn langt';
+                        document.getElementById('opret-password').className = "form-control error";
+                    }
+                    if (errorNumber == 'passwordNullSymbol') {
+                        this.errorMessage = 'Dit password skal indholde specialtegn!!!!!';
+                        document.getElementById('opret-password').className = "form-control error";
+                    }
                     else {
-                        console.log('errorNumber ?? er blevet lavet om? LAD VÆR MED AT ØDELÆGGE MIN KODE! SKAL GIVE ET FRA 1 til 12')
+                       
+                        console.log(errorNumber)
                     }
                 }
             }
